4aba6e97f040e8fc327bf0cdad6e3f442650f0aa
ocviller <ocviller@student.42.fr>
2025-12-09 18:32:31 +0100

    sympa le cpp

--- ex01/Search.cpp
+++ ex01/Search.cpp
@@ -6,7 +6,7 @@
 /*   By: ocviller <ocviller@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/09 17:37:22 by ocviller          #+#    #+#             */
-/*   Updated: 2025/12/09 18:26:03 by ocviller         ###   ########.fr       */
+/*   Updated: 2025/12/09 18:31:52 by ocviller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,33 +33,6 @@ void check_l(std::string str, int len)
     }
 }
 
-int remain(PhoneBook *phone)
-{
-    int ret = 0;
-    
-    if ((phone->contact_list[0].first_name.substr(9).length()) > 0)
-    {
-        std::cout << phone->contact_list[0].first_name.substr(9);
-        std::cout << ".";
-        ret++;
-    }
-    if ((phone->contact_list[0].last_name.substr(9).length()) > 0)
-    {
-        std::cout << phone->contact_list[0].last_name.substr(9);
-        std::cout << ".";
-        ret++;
-    }
-    if ((phone->contact_list[0].nickname.substr(9).length()) > 0)
-    {
-        std::cout << phone->contact_list[0].nickname.substr(9);
-        std::cout << ".";
-        ret++;
-    }
-    if (ret > 0)
-        std::cout << "\n";
-    return (ret);
-}
-
 void printbook(PhoneBook *phone)
 {
     int len;
@@ -78,10 +51,5 @@ void printbook(PhoneBook *phone)
         std::cout << "|";
         check_l(phone->contact_list[0].nickname, len);
         std::cout << "\n";
-        while (1)
-        {
-            if (remain(phone) == 0)
-                break;
-        }
     }
 }
